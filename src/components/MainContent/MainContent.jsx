8119070315ad03099b6288074fb9d56f662a36c1
Pugga <aezakmi2508@gmail.com>
2022-12-12 21:42:02 +0200

    Fix

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -3,7 +3,7 @@ import "./maincontent.css";
 
 import {Swiper, SwiperSlide} from 'swiper/react';
 
-import { Navigation, Pagination, EffectFade, Scrollbar } from 'swiper';
+import { Navigation, Pagination, EffectFade} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
