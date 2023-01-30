56951b573d698dc852ba0d49ff80e344fecf44f0
Pugga <aezakmi2508@gmail.com>
2023-01-30 18:49:56 +0300

    Added Services Form

---
+++ src/components/ModalServices/ModalServices.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./modalservices.css";
+
+const ModalServices = (props) => {
+  const [clientName, setClientName] = useState("");
+  const [clientPhone, setClientPhone] = useState("");
+  const [clientCompany, setClientCompany] = useState("");
+  const [clientGoal, setClientGoal] = useState("");
+  const [clientText, setClientText] = useState("");
+ 
+
+  const botToken = "5987621859:AAG4xA4TVnmztLIebEQJmR_t47I61Z9IUmU";
+  const urlApi = `https://api.telegram.org/bot${botToken}/sendMessage`;
+  const chatId = "-1001565514969";
+  const [success, setSuccess] = useState("");
+
+  const sendAlert = (e) => {
+    e.preventDefault();
+
+    let message = `<b>🤩Якась крута фраза😂😅</b>\n`;
+    message += `<b>Им'я кліента:</b> ${clientName}\n`;
+    message += `<b>Компанія:</b> ${clientCompany}\n`;
+    message += `<b>Мета:</b> ${clientGoal}\n`;
+    message += `<b>Текст кліента:</b> ${clientText}\n`;
+    message += `<b>Телефон клиента:</b><code> ${clientPhone}</code>`;
+    axios
+      .post(urlApi, {
+        chat_id: chatId,
+        parse_mode: "html",
+        text: message,
+      })
+      .then((res) => {
+        setSuccess(true);
+      })
+      .catch((err) => {
+        setSuccess(false);
+      })
+      .finally(() => {});
+  };
+
+  return (
+    <>
+      {success ? (
+        <div className="modalWrapper">
+          <div className="modalCallbackBody">
+            <img
+              src="./images/cross.png"
+              className="closeModal"
+              alt="Close"
+              width="24"
+              height="24"
+              onClick={props.closeModal}
+            />
+            
+              <span className="successAlert">Дані успішно<br/> відправлені. З вами зв'яжеться<br/> менеджер протягом 30 хвилин.</span>
+            
+          </div>
+        </div>
+      ) : (
+        <div className="modalWrapper">
+          <div className="modalCallbackBody">
+            <img
+              src="./images/cross.png"
+              className="closeModal"
+              alt="Close"
+              width="24"
+              height="24"
+              onClick={props.closeModal}
+            />
+            <div className="callbackFormContent">
+              <h4>{props.services}</h4>
+              <p>
+              Будь ласка заповніть форму
+              </p>
+              <form action="" className="callbackForm" onSubmit={sendAlert}>
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Ім'я"
+                  value={clientName}
+                  onChange={(event) => setClientName(event.target.value)}
+                />
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Компанія"
+                  value={clientCompany}
+                  onChange={(event) => setClientCompany(event.target.value)}
+                />
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Що плануемо знімати?)"
+                  value={clientGoal}
+                  onChange={(event) => setClientGoal(event.target.value)}
+                />
+                <input
+                  type="tel"
+                  name="phone"
+                  placeholder="Номер телефону"
+                  value={clientPhone}
+                  onChange={(event) => setClientPhone(event.target.value)}
+                />
+                <textarea
+                
+                  type="name"
+                  name="name"
+                  placeholder="Що нам потрібно знати?)"
+                  value={clientText}
+                  onChange={(event) => setClientText(event.target.value)}
+                />
+                <button className="callbackModalBtn" type="submit">
+                  ОТПРАВИТЬ
+                </button>
+              </form>
+              <span className="policyTitle">
+                Нажимая кнопку «Отправить», Вы соглашаетесь с<br />
+                <p>политикой конфиденциальности</p>
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ModalServices;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,21 +1,54 @@
 import React from "react";
 import "./services.css";
+import { useState } from "react";
+import ModalServices from "../ModalServices/ModalServices";
 
 const Services = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [servicesName, setServicesName] = useState();
+
+
+  console.log(modalOpen)
+  const openFormServices1 = () => {
+   
+
+    setServicesName("Content");
+    setModalOpen(true);
+
+    console.log(servicesName);
+  };
+  const openFormServices2 = () => {
+    
+    setServicesName("Fashion");
+    setModalOpen(true);
+
+    console.log(servicesName);
+    
+  };
+  const openFormServices3 = () => { 
+    setModalOpen(true);
+    setServicesName("Reels");
+   
+
+    console.log(modalOpen);
+  };
   return (
-    <div className="services">
-      <div className="servicesWrapper">
-        <div className="serviceItem">
-          <p>Зйомка контенту</p>
-        </div>
-        <div className="serviceItem">
-          <p>Іміджева реклама</p>
-        </div>
-        <div className="serviceItem">
-          <p>Зйомка рілз</p>
+    <>
+     { modalOpen ? <ModalServices services={servicesName} closeModal={() => setModalOpen(false)} /> : null }
+      <div className="services">
+        <div className="servicesWrapper">
+          <div className="serviceItem" onClick={openFormServices1}>
+            <p>Зйомка контенту</p>
+          </div>
+          <div className="serviceItem" onClick={openFormServices2}>
+            <p>Іміджева реклама</p>
+          </div>
+          <div className="serviceItem" onClick={openFormServices3}>
+            <p>Зйомка рілз</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Services;
